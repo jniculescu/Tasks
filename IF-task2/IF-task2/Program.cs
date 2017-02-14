b778a364b2805661b20fb2c4cffc6da3deca110e
jniculescu <jani.niculescu@gmail.com>
2017-02-14 14:01:12 +0200

    IF Task 2 done

---
+++ IF-task2/IF-task2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+  Jani Niculescu
+  14.2.2017
+  IF task 2
+ */
+
+namespace IF_task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.Write("Anna numero : ");
+            i = int.Parse(Console.ReadLine());
+
+            if (i % 2 == 1)
+            {
+                Console.WriteLine("Luku on pariton");
+                Console.ReadKey();
+            }
+
+            else
+            {
+                Console.WriteLine("Luku on parillinen");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}

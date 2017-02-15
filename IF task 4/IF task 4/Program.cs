04e59815f4b03f34b9f5ec555d22e5a2c49000ce
jniculescu <jani.niculescu@gmail.com>
2017-02-15 10:42:33 +0200

    Task 4 complete

--- IF task 4/IF task 4/Program.cs
+++ IF task 4/IF task 4/Program.cs
@@ -10,14 +10,23 @@ namespace IF_task_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Anna 3 Numeroa: ");
+            Console.WriteLine("Anna 3 yksittäistä numeroa : ");
             int[] arr = new int[3];
+
             for(int i = 0; i < arr.Length; i++)
             {
                 arr[i] = int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine(arr[0]);
+            Array.Sort(arr);
+            foreach (int value in arr)
+            {
+                Console.Write(value + " ");
+            }
+
+            Console.ReadKey();
+
+
         }
     }
 }

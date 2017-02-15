3e1f99ab69f9e3bccb541769d27d8f1fb6f05f31
jniculescu <jani.niculescu@gmail.com>
2017-02-15 14:20:39 +0200

    files updated

---
+++ basics/IF/IF task 4/IF task 4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IF_task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Anna 3 yksittäistä numeroa : ");
+            int[] arr = new int[3];
+
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(arr);
+
+            Console.Write("Annoit Numerot ");
+
+            foreach (int value in arr)
+            {
+
+                Console.Write( value + " ");
+            }
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ basics/IF/IF-task/IF-task/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ Jani Niculescu
+ IF statement Task 1
+ 14,2.2017
+ */
+
+namespace IF_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            Console.WriteLine("Anna numero");
+            i = int.Parse(Console.ReadLine());
+
+            if (i == 0)
+            {
+                Console.WriteLine("Numero on nolla");
+                Console.ReadKey();
+            }
+
+            if (i <= -1)
+            {
+                Console.WriteLine("Numero" + i + " on negatiivinen");
+                Console.ReadKey();
+            }
+
+            if (i >= 1)
+            {
+                Console.WriteLine("Numero" + i + " on positiivinen");
+                Console.ReadKey();
+            }
+        }
+    }
+}

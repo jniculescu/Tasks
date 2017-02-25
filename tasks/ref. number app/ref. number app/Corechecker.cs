2f2f88ea9a9714d3349e155464e4caf7a5253efa
jniculescu <jani.niculescu@gmail.com>
2017-02-25 19:50:37 +0200

    updated frame for ref number app.

---
+++ tasks/ref. number app/ref. number app/Corechecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ref._number_app
+{
+    class Corechecker
+{
+    string refnum;
+
+    
+
+}
+}

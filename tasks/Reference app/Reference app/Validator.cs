4c6dcc3dd0848c96743dafb1e16dfe2f642d02e5
jniculescu <jani.niculescu@gmail.com>
2017-02-26 23:35:34 +0200

    Refrence app framework redone. Navigation between forms done.

---
+++ tasks/Reference app/Reference app/Validator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reference_app
+{
+    public partial class Validator : Form
+    {
+        public Validator()
+        {
+            InitializeComponent();
+        }
+
+        private void Validator_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 main = new Form1();
+            main.Show();
+            Visible = false;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
